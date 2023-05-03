afa63df840b1e119b6a990f3946f6f3a7f5737c2
Aki.Yano <orihika0123@gmail.com>
2023-05-03 14:29:54 +0900

    feat: MyReadonlyを追加

--- playground/00007-easy-readonly.ts
+++ playground/00007-easy-readonly.ts
@@ -29,7 +29,9 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key]
+}
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
