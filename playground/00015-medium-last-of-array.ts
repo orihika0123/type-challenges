70dfb45f55d2e5cbdcdfd8af7ca6aa8a0ab3d319
Aki.Yano <orihika0123@gmail.com>
2023-05-04 14:58:39 +0900

    feat: Lastを追加

--- playground/00015-medium-last-of-array.ts
+++ playground/00015-medium-last-of-array.ts
@@ -24,7 +24,7 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type Last<T extends any[]> = any
+type Last<T extends any[]> = T extends [...any, infer L] ? L : never
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
